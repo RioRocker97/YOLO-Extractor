20dfbc1207dcee2250c27a361bbda7f4510bf996
RioRocker97 <changsmarter@gmail.com>
2023-02-03 15:50:10 +0700

    server-side

--- me.py
+++ me.py
@@ -1,17 +1,19 @@
 from ultralytics import YOLO
 import cv2
 import numpy as np
-
-IMAGE_FILE = "./2.jpg"
-
-model = YOLO("yolov8l-seg.pt")
+import requests
+from base64 import b64decode
+from os import environ
+IMAGE_FILE = "./6.jpg"
+CLIP_DROP_API_KEY = environ['CLIP_DROP']
+MODEL = YOLO("yolov8l-seg.pt")
+BGD_MODEL = np.zeros((1,65),np.float64)
+FGD_MODEL = np.zeros((1,65),np.float64)
 cv_image = cv2.imread(IMAGE_FILE)
 cv_height,cv_width,_ = cv_image.shape
-res = model.predict(IMAGE_FILE,show=True)
+res = MODEL.predict(IMAGE_FILE)
 human_mark = []
 real_mark = []
-bgdModel = np.zeros((1,65),np.float64)
-fgdModel = np.zeros((1,65),np.float64)
 for obj in res:
     temp = obj.boxes.xyxy
     temp2 = obj.masks.segments
@@ -27,19 +29,46 @@ for each_mark in real_mark:
     contours = np.array(each_mark.astype(int))
     cv2.fillPoly(blank,pts=[contours],color=(255,255,255))
 
-
+all_mask = cv2.cvtColor(blank,cv2.COLOR_RGB2GRAY)
+all_mask = np.where(all_mask == 255 ,cv2.GC_BGD,cv2.GC_FGD).astype('uint8')
+final_full_mask,_,_ = cv2.grabCut(cv_image,all_mask,None,BGD_MODEL,FGD_MODEL,3,cv2.GC_INIT_WITH_MASK)
+final_full_mask = np.where((final_full_mask==2)|(final_full_mask==0),0,1).astype('uint8')
+final_full_img = cv_image*final_full_mask[:,:,np.newaxis]
+cv2.imwrite("mask.png",blank)
 for i,obj in enumerate(human_mark):
     cropped = cv_image[obj[1]:obj[3],obj[0]:obj[2]]
     cropped_mask = blank[obj[1]:obj[3],obj[0]:obj[2]]
     cropped_mask = cv2.cvtColor(cropped_mask,cv2.COLOR_RGB2GRAY)
     cropped_mask = np.where(cropped_mask == 255,cv2.GC_FGD,cv2.GC_BGD).astype('uint8')
-    final_mask,_,_ = cv2.grabCut(cropped,cropped_mask,None,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_MASK)
-
+    final_mask,_,_ = cv2.grabCut(cropped,cropped_mask,None,BGD_MODEL,FGD_MODEL,3,cv2.GC_INIT_WITH_MASK)
     final_mask = np.where((final_mask==2)|(final_mask==0),0,1).astype('uint8')
     final_img = cropped*final_mask[:,:,np.newaxis]
-    cv2.imshow("EXP "+str(i),final_img)
-    cv2.imwrite("EXP"+str(i)+".png",final_img)
+    tmp = cv2.cvtColor(final_img, cv2.COLOR_BGR2GRAY)
+    _, alpha = cv2.threshold(tmp, 0, 255, cv2.THRESH_BINARY)
+    b, g, r = cv2.split(final_img)
+    rgba = [b,g,r, alpha]
+    dst = cv2.merge(rgba,4)
+    #print(dst)
+    # cv2.imshow("EXP "+str(i),dst)
+    cv2.imwrite("EXP"+str(i)+".png",dst)
 
+print("Finished Extraction...")
+"""
+img1 = cv2.imencode('.jpg',cv_image)[1].tobytes()
+img2 = cv2.imencode('.png',blank)[1].tobytes()
+r = requests.post('https://clipdrop-api.co/cleanup/v1',
+  files = {
+    'image_file': ('image.jpg', img1, 'image/jpeg'),
+    'mask_file': ('mask.png', img2, 'image/png')
+    },
+  headers = { 'x-api-key': CLIP_DROP_API_KEY}
+)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+if (r.ok):
+    buff = np.frombuffer(r.content,dtype=np.uint8)
+    buff = cv2.imdecode(buff,flags=1)
+    cv2.imwrite("KEK.jpg",buff)
+  # r.content contains the bytes of the returned image
+else:
+  r.raise_for_status()
+"""
---
+++ photo_api.py
@@ -0,0 +1,101 @@
+import functions_framework
+from flask import jsonify
+from ultralytics import YOLO
+from os import environ
+from base64 import b64encode
+import cv2
+import numpy as np
+import requests
+###### GLOBAL FUNCTION #############
+CLIP_DROP_API_KEY = environ['CLIP_DROP']
+MODEL = YOLO(environ['YOLO_MODEL'])
+BGD_MODEL = np.zeros((1,65),np.float64)
+FGD_MODEL = np.zeros((1,65),np.float64)
+######## Image Segmentation ##############
+def detectAndExtract(binary_base_image):
+    image = byteToImageArray(binary_base_image)
+    img_height,img_width,_ = image.shape
+    human_mark = []
+    real_mark = []
+    human_img = []
+    blank = np.zeros(image.shape,np.uint8)
+    res = MODEL.predict(byteToImageArray(binary_base_image))
+    # Get Prediction Result
+    for obj in res:
+        temp = obj.boxes.xyxy
+        temp2 = obj.masks.segments
+        for i,human_checked in enumerate(obj.numpy().boxes.cls):
+            if int(human_checked) == 0 :
+                human_mark.append(temp[i].numpy().astype(int))
+                real_mark.append(temp2[i])
+    # Prepare Human Mask
+    for each_mark in real_mark:
+        for j in range(0,len(each_mark)):
+            each_mark[j][0] *= img_width
+            each_mark[j][1] *= img_height
+        contours = np.array(each_mark.astype(int))
+        cv2.fillPoly(blank,pts=[contours],color=(255,255,255))
+    # Extract Human Mask
+    for obj in human_mark:
+        cropped = image[obj[1]:obj[3],obj[0]:obj[2]]
+        cropped_mask = blank[obj[1]:obj[3],obj[0]:obj[2]]
+        final_cut = makeMask(cropped,cropped_mask)
+        final_cut = makeBlackTransparent(final_cut)
+        human_img.append(imageArrayToBase64String(final_cut))
+    return blank,human_img
+######## Background Removal ##############
+def cleanUpOriginalImage(binary_base_image,human_mask_all):
+    return imageArrayToBase64String(human_mask_all)
+    """
+    image = byteToImageArray(binary_base_image)
+    image = cv2.imencode('.jpg',image)[1].tobytes()
+    blank = cv2.imencode('.png',human_mask_all)[1].tobytes()
+    r = requests.post('https://clipdrop-api.co/cleanup/v1',
+    files = {
+        'image_file': ('image.jpg', image, 'image/jpeg'),
+        'mask_file': ('mask.png', blank, 'image/png')
+        },
+    headers = { 'x-api-key': CLIP_DROP_API_KEY}
+    )
+    if (r.ok):
+        return imageArrayToBase64String(byteToImageArray(r.content),'.jpg')
+    else:
+        r.raise_for_status()
+    """
+######## Byte Data To NP Array Helper ############
+def byteToImageArray(byte_data):
+    buff = np.frombuffer(byte_data,dtype=np.uint8)
+    buff = cv2.imdecode(buff,flags=1)
+    return buff
+######## NP Array To Byte Data Helper ############
+def imageArrayToByte(image_array,file_type):
+    return cv2.imencode(file_type,image_array)[1].tobytes()
+def imageArrayToBase64String(image_array,file_type='.png'):
+    prefix = 'data:image/png;base64,' if file_type == '.png' else 'data:image/jpeg;base64,'
+    return prefix+b64encode(cv2.imencode(file_type,image_array)[1]).decode()
+######## GrabCut Helper ############
+def makeMask(image,mask,alpha=255):
+    tmp = cv2.cvtColor(mask,cv2.COLOR_RGB2GRAY)
+    tmp = np.where(tmp == alpha ,1,0).astype('uint8')
+    return cv2.bitwise_and(image,image,mask=tmp)
+######## Transparent BG Helper ##############
+def makeBlackTransparent(image):
+    tmp = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    _,alpha = cv2.threshold(tmp,0,255,cv2.THRESH_BINARY)
+    b,g,r = cv2.split(image)
+    rgba = [b,g,r,alpha]
+    return cv2.merge(rgba,4)
+@functions_framework.http
+def photo_api(request):
+    if request.method == 'GET' :
+        return 'OK'
+    elif request.method == 'POST' :
+        image_file = request.files['original'].read()
+        human_mask_all,human_pose = detectAndExtract(image_file)
+        image_no_human = cleanUpOriginalImage(image_file,human_mask_all)
+        return jsonify({
+            'base':image_no_human,
+            'human_pose':human_pose
+        })
+    else :
+        return "Not Found",404
